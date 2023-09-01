a6b4387e547c47d48c4bb789f09f795bd39bcef4
yukipo~ <>
2023-09-01 19:41:02 +0900

    スマートポインタ実装途中

--- Pass&Over/code/GameMgr/GameMgr.cpp
+++ Pass&Over/code/GameMgr/GameMgr.cpp
@@ -1,2 +1,26 @@
 #include "GameMgr.h"
 #include<DxLib.h>
+#include<memory>
+
+#include"../GameSetting/GameSetting.h"
+
+// コンストラクタ //
+
+GameMgr::GameMgr()
+{
+    std::shared_ptr<GameSetting> gmSet(new GameSetting);
+}
+
+// デストラクタ //
+
+GameMgr::~GameMgr()
+{
+
+}
+
+// 処理 //
+
+void GameMgr::Procces()
+{
+	
+}
