e7b03b4071152bc505d09eab304d9bf9d77d2db3
yukipo~ <>
2023-09-08 19:38:36 +0900

    プレイシーンファイル追加

---
+++ Pass&Over/code/Scene/Play/Play.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/*Playクラス*/
+class Play:public SceneBase
+{
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    Play();
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~Play();
+
+    /// <summary>
+    /// シーン更新処理
+    /// </summary>
+    /// <param name="deltaTime">:フレームレート</param>
+    /// <returns>次フレームのシーン</returns>
+    SceneBase* SceneUpdate(const float& deltaTime)override;
+
+    /// <summary>
+    /// シーン描画
+    /// </summary>
+    void SceneDraw()override;
+};
+
