8e96cb4ace105fb3f715b2121ba32b3b6fe31c99
yukipo~ <>
2023-09-11 19:48:49 +0900

    オブジェクト管理処理実装途中

--- Pass&Over/code/Object/ObjMgr/ObjMgr.cpp
+++ Pass&Over/code/Object/ObjMgr/ObjMgr.cpp
@@ -1,1 +1,77 @@
+#include<assert.h>
+
+#include"../ObjBase/ObjBase.h"
+#include"../ObjBase/ObjTag.h"
 #include "ObjMgr.h"
+
+std::shared_ptr<class ObjMgr> ObjMgr::objMgr = nullptr;
+
+// オブジェクトマネージャー初期化処理 //
+
+void ObjMgr::InitObjMgr()
+{
+    //自身の中身が空だったらインスタンス生成
+    if (!objMgr.get())
+    {
+        objMgr = std::make_shared<class ObjMgr>(new ObjMgr);
+    }
+}
+
+// オブジェクト追加処理 //
+
+void ObjMgr::AddObj(ObjBase* newObj)
+{
+    objMgr->object.emplace(newObj->GetTag(), newObj);
+}
+
+// オブジェクト更新処理 //
+
+void ObjMgr::UpdateObj(const float& deltaTime)
+{
+    //全タグ分更新処理をまとめて行う
+    for (auto& tag : objTagAll)
+    {
+        for (int i = 0; i < objMgr->object[tag].size(); i++)
+        {
+            objMgr->object[tag][i]->Update(deltaTime);
+        }
+    }
+}
+
+// オブジェクト死亡処理 //
+
+void ObjMgr::OnDeadObj()
+{
+    //全タグ分死亡オブジェクトを探して削除
+    for (auto& tag : objTagAll)
+    {
+        for (auto& dead:objMgr->object[tag])
+        {
+            //死んでいたらオブジェクト削除
+            if (!dead->IsAlive())
+            {
+                DeleteObj(dead.get());
+            }
+        }
+    }
+}
+
+// オブジェクト削除処理 //
+
+void ObjMgr::DeleteObj(ObjBase* unnecObj)
+{
+    //削除オブジェクトのタグ取得
+    ObjTag tag = unnecObj->GetTag();
+
+    //オブジェクトを検索
+    auto iter = find(objMgr->object[tag].begin(),
+        objMgr->object[tag].end(), unnecObj);
+    assert(iter == objMgr->object[tag].end());
+
+    //見つかったら末尾に移動させて削除
+    if (iter != objMgr->object[tag].end())
+    {
+        std::iter_swap(iter, objMgr->object[tag].end() - 1);
+        objMgr->object[tag].back().reset();
+    }
+}
--- Pass&Over/code/Object/ObjMgr/ObjMgr.h
+++ Pass&Over/code/Object/ObjMgr/ObjMgr.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<memory>
+#include<vector>
+#include<unordered_map>
 
 /*ObjMgrクラス*/
 class ObjMgr final
@@ -8,28 +10,39 @@ public:
     /// <summary>
     /// オブジェクトマネージャー初期化処理
     /// </summary>
-    void InitObjMgr();
+    static void InitObjMgr();
 
     /// <summary>
     /// オブジェクト追加処理
     /// </summary>
-    void AddObj();
+    static void AddObj(ObjBase* newObj);
 
     /// <summary>
     /// オブジェクト更新処理
     /// </summary>
     /// <param name="deltaTime">:フレームレート</param>
-    void UpdateObj(const float& deltaTime);
+    static void UpdateObj(const float& deltaTime);
+
+    /// <summary>
+    /// オブジェクト死亡処理
+    /// </summary>
+    static void OnDeadObj();
 
     /// <summary>
     /// オブジェクト描画
     /// </summary>
-    void DrawObj();
+    static void DrawObj();
+
+    /// <summary>
+    /// オブジェクト削除処理
+    /// </summary>
+    /// <param name="unnecObj">:不必要オブジェクト</param>
+    static void DeleteObj(ObjBase* unnecObj);
 
     /// <summary>
     /// 全オブジェクト削除処理
     /// </summary>
-    void DeleteAllObj();
+    static void DeleteAllObj();
 private:
     /// <summary>
     /// コンストラクタ(シングルトン)
@@ -41,6 +54,9 @@ private:
     /// </summary>
     ~ObjMgr();
 
-    std::shared_ptr<ObjMgr> objMgr;     //自身の実体
+    static std::shared_ptr<class ObjMgr> objMgr;     //自身の実体
+
+    std::unordered_map<ObjTag, std::vector<
+        std::shared_ptr<class ObjBase>>> object;     //オブジェクト
 };
 
