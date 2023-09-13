3d09511075142741d0e080cb55412151ab1ca312
yukipo~ <>
2023-09-13 12:24:15 +0900

    Playerファイル生成

---
+++ Pass&Over/code/Object/CharaObj/Player/Player.cpp
@@ -0,0 +1,28 @@
+#include<DxLib.h>
+
+#include"../CharaObjBase/CharaObjBase.h"
+#include "Player.h"
+
+// コンストラクタ //
+
+Player::Player()
+{
+
+}
+
+// デストラクタ //
+
+Player::~Player()
+{
+
+}
+
+// 更新処理 //
+
+void Player::Update(const float& deltaTime)
+{
+    if (CheckHitKey(KEY_INPUT_P))
+    {
+        isAlive
+    }
+}
---
+++ Pass&Over/code/Object/CharaObj/Player/Player.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/*Playerクラス*/
+class Player:public CharaObjBase
+{
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    Player();
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~Player();
+
+private:
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    /// <param name="deltaTime">フレームレート</param>
+    void Update(const float& deltaTime)override;
+
+    /// <summary>
+    /// 描画処理
+    /// </summary>
+    void Draw()override;
+};
+
