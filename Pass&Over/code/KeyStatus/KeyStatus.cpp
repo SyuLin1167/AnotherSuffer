58c0b89975b1fa61c18da62464c52c9d1ca7f397
yukipo~ <>
2023-11-01 12:20:25 +0900

    キー入力判定処理途中

--- Pass&Over/code/KeyStatus/KeyStatus.cpp
+++ Pass&Over/code/KeyStatus/KeyStatus.cpp
@@ -1,28 +1,32 @@
-//#include "KeyStatus.h"
-//
-//std::unique_ptr<KeyStatus> KeyStatus::singleton = nullptr;
-//
-//void KeyStatus::CreateInstance()
-//{
-//    //自身の中身が空だったらインスタンス生成
-//    if (!singleton)
-//    {
-//        singleton.reset(new KeyStatus);
-//    }
-//}
-//
-//KeyStatus::KeyStatus()
-//    :inputState(0)
-//{
-//}
-//
-//bool IsInput(int key)
-//{
-//    //入力してたらtrueしていなかったらfalseを返す
-//    if (key != 0)
-//    {
-//        return true;
-//    }
-//
-//    return false;
-//}
+#include<DxLib.h>
+#include "KeyStatus.h"
+
+std::unique_ptr<KeyStatus> KeyStatus::singleton = nullptr;
+
+void KeyStatus::CreateInstance()
+{
+    //自身の中身が空だったらインスタンス生成
+    if (!singleton)
+    {
+        singleton.reset(new KeyStatus);
+    }
+}
+
+
+KeyStatus::KeyStatus()
+{
+    //処理なし
+}
+
+void KeyStatus::KeyStateDecision(int key, float deltaTime)
+{
+    auto findKey = std::find(keyData.begin(), keyData.end(), key);
+
+    if (findKey != keyData.end())
+    {
+        if (CheckHitKey(key))
+        {
+
+        }
+    }
+}
--- Pass&Over/code/KeyStatus/KeyStatus.h
+++ Pass&Over/code/KeyStatus/KeyStatus.h
@@ -1,48 +1,57 @@
-//#pragma once
-//#include<DxLib.h>
-//#include<memory>
-//
-///// <summary>
-///// KeyStatusクラス
-///// </summary>
-//class KeyStatus
-//{
-//public:
-//    /// <summary>
-//    /// キー入力初期化処理
-//    /// </summary>
-//    void CreateInstance();
-//
-//    /// <summary>
-//    /// 入力判定処理
-//    /// </summary>
-//    /// <param name="key">:判定したいキー</param>
-//    /// <returns>入力:true|未入力:false</returns>
-//    bool IsInput(int key);
-//
-//    /// <summary>
-//    /// デストラクタ
-//    /// </summary>
-//    ~KeyStatus();
-//private:
-//    /// <summary>
-//    /// コンストラクタ(シングルトン)
-//    /// </summary>
-//    KeyStatus();
-//
-//    /// <summary>
-//    /// 入力状態種類
-//    /// </summary>
-//    enum class KeyState
-//    {
-//        NOMAL=0,
-//        ONINPUT,
-//        NOWINPUT,
-//        UNINPUT,
-//    }keyState;
-//
-//    static std::unique_ptr<KeyStatus> singleton;        //自身の実体
-//
-//    int inputState;                                     //入力ステータス
-//};
-//
+#pragma once
+#include<vector>
+#include<memory>
+
+constexpr int UNINPUT = 0x0001;     //未入力時
+constexpr int NOWUNINPUT = 0x0002;  //未入力中
+constexpr int ONINPUT = 0x0004;     //入力時
+constexpr int NOWINPUT = 0x0008;    //入力中
+
+/// <summary>
+/// KeyStatusクラス
+/// </summary>
+class KeyStatus final
+{
+public:
+    /// <summary>
+    /// キー入力初期化処理
+    /// </summary>
+    void CreateInstance();
+
+    /// <summary>
+    /// 入力判定処理
+    /// </summary>
+    /// <param name="key">:判定したいキー</param>
+    /// <param name="deltaTime">:デルタタイム</param>
+    void KeyStateDecision(int key, float deltaTime);
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~KeyStatus();
+private:
+    /// <summary>
+    /// コンストラクタ(シングルトン)
+    /// </summary>
+    KeyStatus();
+
+    static std::unique_ptr<KeyStatus> singleton;        //自身の実体
+
+    const float MAXCOUNT = 1.0f;
+    const float MINCOUNT = -1.0f;
+
+    /// <summary>
+    /// キーパラメーター
+    /// </summary>
+    struct KeyParam
+    {
+    public:
+        KeyParam();
+
+        int inputState;             //入力ステータス
+        float inputCount;           //入力カウント
+    };
+
+    std::vector<KeyParam> keyData;  //キーデータ
+};
+
