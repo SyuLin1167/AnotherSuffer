a8664742d6e7372e4b448ec686ecd8c325ebb1c8
yukipo~ <>
2023-11-04 16:28:06 +0900

    シーン切り替え修正

--- Pass&Over/code/Scene/Play/Play.cpp
+++ Pass&Over/code/Scene/Play/Play.cpp
@@ -1,9 +1,11 @@
 #include<DxLib.h>
 
+#include"../../KeyStatus/KeyStatus.h"
 #include"../../Object/ObjManager/ObjManager.h"
 #include"../../Object/Camera/Camera.h"
 #include"../../Object/CharaObj/Player/Player.h"
 #include"../SceneBase/SceneBase.h"
+#include"../Title/Title.h"
 #include "Play.h"
 
 Play::Play()
@@ -19,12 +21,22 @@ Play::~Play()
 
 SceneBase* Play::UpdateScene(const float deltaTime)
 {
+    //オブジェクト更新
     ObjManager::UpdateObj(deltaTime);
+
+    //シーン切り替え
+    if (KeyStatus::KeyStateDecision(KEY_INPUT_RETURN, ONINPUT))
+    {
+        ObjManager::DeleteAllObj();
+        return new Title;
+    }
+
     return this;
 }
 
 void Play::DrawScene()
 {
+    //オブジェクト描画
     ObjManager::DrawObj();
     DrawFormatString(0, 0, GetColor(255, 255, 255), "play");
 }
