b5ba7a1cb0fa2e39b7bbd83172a9b22b0afdb49d
yukipo~ <>
2023-11-18 12:23:01 +0900

    プロジェクト切り替え

--- AnotherSuffer/code/Scene/Play/Play.cpp
+++ AnotherSuffer/code/Scene/Play/Play.cpp
@@ -38,5 +38,10 @@ void Play::DrawScene()
 {
     //オブジェクト描画
     ObjManager::DrawObj();
+    for (int i = -20; i < 20; i++)
+    {
+        DrawLine3D(VGet(-200, 0, i * 10), VGet(200, 0, i * 10), GetColor(255, 255, 255));
+        DrawLine3D(VGet(i*10, 0, -200), VGet(i*10, 0, 200), GetColor(255, 255, 255));
+    }
     DrawFormatString(0, 0, GetColor(255, 255, 255), "play");
 }
