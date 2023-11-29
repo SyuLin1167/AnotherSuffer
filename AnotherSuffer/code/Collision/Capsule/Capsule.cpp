446aed033d171b8230c38b76294a1d8a997a6b49
yukipo~ <>
2023-11-30 00:28:25 +0900

    当たり判定実装、押し戻し量実装途中

--- AnotherSuffer/code/Collision/Capsule/Capsule.cpp
+++ AnotherSuffer/code/Collision/Capsule/Capsule.cpp
@@ -27,22 +27,33 @@ bool Capsule::OnCollisionWithMesh(const int modelHandle, MV1_COLL_RESULT_POLY_DI
 {
     //“–‚½‚è”»’èî•ñ‚©‚ç”»’èŒ‹‰Ê‚ð•Ô‚·
     colInfo = MV1CollCheck_Capsule(modelHandle, -1, worldStart, worldEnd, radius);
-    if (colInfo.HitNum > 0)
+    if (colInfo.HitNum == 0)
     {
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 VECTOR Capsule::CalcPushBackFromMesh(const MV1_COLL_RESULT_POLY_DIM& colInfo)
 {
+    VECTOR nomalVec;
 
-    return VECTOR();
+    for (int i = 0; i < colInfo.HitNum; i++)
+    {
+        nomalVec = colInfo.Dim->Normal;
+
+        if (HitCheck_Capsule_Triangle(worldStart, worldEnd,
+            radius, colInfo.Dim[i].Position[0],colInfo.Dim[i].Position[1],colInfo.Dim[i].Position[2]))
+        {
+
+        }
+    }
+    return VECTOR(0,0,0);
 }
 
 void Capsule::DrawDebug()
 {
-    DrawCapsule3D(worldStart, worldEnd, radius, 8, GetColor(255, 50, 255), GetColor(255, 50, 255), false);
+    DrawCapsule3D(worldStart, worldEnd, radius, 8, GetColor(255, 255, 255), GetColor(255, 50, 255), false);
 }
 
 
